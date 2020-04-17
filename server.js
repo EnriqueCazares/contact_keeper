ef511cbd629fea56bdaf1476d1b5454611620692
Enrique Cazares <cazaresenrique97@gmail.com>
2020-04-17 16:29:28 -0500

    Commit Inicial

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) =>
+  res.json({ msg: 'Welcome to the ContactKeeper API...' })
+);
+
+// Mount Routes
+const users = require('./routes/users');
+const contacts = require('./routes/contacts');
+const auth = require('./routes/auth');
+
+// Define Routes
+app.use('/api/users', users);
+app.use('/api/contacts', contacts);
+app.use('/api/auth', auth);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
